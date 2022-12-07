514ac92ac307aa19dd7d7560d20414ee2204204e
bilecky <bilecky95@gmail.com>
2022-12-08 00:28:48 +0100

    task 3 - create login form

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+
+@Component({
+  selector: 'app-login',
+  styleUrls: ['./login.component.scss'],
+  templateUrl: './login.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+  readonly login: FormGroup = new FormGroup({ username: new FormControl(), password: new FormControl() });
+
+  constructor(private _loginService: LoginService) {
+  }
+
+  onLoginSubmitted(login: FormGroup): void {
+    this._loginService.create({
+      username: login.get('username')?.value,
+      password: login.get('password')?.value
+    }).subscribe();
+  }
+}
