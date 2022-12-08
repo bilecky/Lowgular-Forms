dd0b3808ffa5a56884de72f88b78b23aa770ba83
bilecky <bilecky95@gmail.com>
2022-12-08 01:56:28 +0100

    task 4 - extend product form with categories list

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,13 +1,16 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-product-form',
   styleUrls: ['./product-form.component.scss'],
   templateUrl: './product-form.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [CategoryService]
 })
 export class ProductFormComponent {
   readonly productForm: FormGroup = new FormGroup({
@@ -17,9 +20,11 @@ export class ProductFormComponent {
     category: new FormControl(),
     image: new FormControl()
   });
+  readonly list$: Observable<any> = this._categoryService.getAll();
 
-  constructor(private _productService: ProductService) {
+  constructor(private _productService: ProductService, private _categoryService: CategoryService) {
   }
+
 
   onProductFormSubmitted(productForm: FormGroup): void {
     this._productService.create({
